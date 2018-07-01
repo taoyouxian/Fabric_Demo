6f1e72c00bd095c995893daa8a1e385fb19fb43d
u1095279159872 <1095279159872@tianchi.com>
2018-07-01 19:29:06 +0800

    add basic function

---
+++ chaincode/container-app/container-chaincode.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	sc "github.com/hyperledger/fabric/protos/peer"
+)
+
+type SmartContract struct {
+}
+
+type Container struct {
+	X string `json:"x"`
+}
+
+func (s *SmartContract) Init(APIstub shim.ChaincodeStubInterface) sc.Response {
+	return shim.Success(nil)
+}
+
+func (s *SmartContract) Invoke(APIstub shim.ChaincodeStubInterface) sc.Response {
+	
+	function, args := APIstub.GetFunctionAndParameters()
+
+	if function == "addRecord" {
+		return s.addRecord(APIstub, args)
+	} else if function == "getRecord" {
+		return s.getRecord(APIstub,args)
+	} else if function == "encRecord" {
+		return s.encRecord(APIstub,args)
+	} else if function == "decRecord" {
+		return s.decRecord(APIstub,args)
+	}
+
+	return shim.Error("Invalid Smart Contract function name.")
+}
+
+func (s *SmartContract) addRecord(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+
+	if len(args) != 4 {
+		return shim.Error("Incorrect number of arguments. Expecting 4")
+	}
+	containerAsBytes, _ := json.Marshal(args[1])
+
+	err:=APIstub.PutState(args[0],containerAsBytes)
+	if err != nil{
+		return shim.Error("wirteIn error")
+	}
+
+	return shim.Success(nil)
+}
+
+func (s *SmartContract) getRecord(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+
+	if len(args) != 2 {
+		return shim.Error("Incorrect number of arguments. Expecting 2")
+	}
+		
+	containerAsBytes, _ := APIstub.GetState(args[0])
+	if containerAsBytes == nil {
+		return shim.Error("Could not locate container")
+	}
+	return shim.Success(containerAsBytes)
+}  
+
+func (s *SmartContract) encRecord(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+
+	if len(args) != 4 {
+		return shim.Error("Incorrect number of arguments. Expecting 4")
+	}
+	containerAsBytes, _ := json.Marshal(args[1])
+
+	err:=APIstub.PutState(args[0],containerAsBytes)
+	if err != nil{
+		return shim.Error("wirteIn error")
+	}
+
+	return shim.Success(nil)
+}
+
+func (s *SmartContract) decRecord(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+
+	if len(args) != 2 {
+		return shim.Error("Incorrect number of arguments. Expecting 2")
+	}
+		
+	containerAsBytes, _ := APIstub.GetState(args[0])
+	if containerAsBytes == nil {
+		return shim.Error("Could not locate container")
+	}
+	return shim.Success(containerAsBytes)
+}  
+ 
+func main() {
+	err := shim.Start(new(SmartContract))
+	if err != nil {
+		fmt.Printf("Error creating new Smart Contract: %s", err)
+	}
+}
